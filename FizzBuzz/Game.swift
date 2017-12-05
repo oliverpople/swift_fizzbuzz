54f450c4d98b15e441271d2088b78bdd6ea98532
oliverpople <oliverpople@gmail.com>
2017-12-05 07:40:25 +0000

    Added Game class

---
+++ FizzBuzz/Game.swift
@@ -0,0 +1,11 @@
+//
+//  Game.swift
+//  FizzBuzz
+//
+//  Created by Oliver Pople on 05/12/2017.
+//  Copyright © 2017 Oliver Pople. All rights reserved.
+//
+
+class Game {
+    
+}
---
+++ FizzBuzzTests/GameTests.swift
@@ -0,0 +1,23 @@
+//
+//  GameTests.swift
+//  FizzBuzz
+//
+//  Created by Oliver Pople on 05/12/2017.
+//  Copyright © 2017 Oliver Pople. All rights reserved.
+//
+import XCTest
+@testable import FizzBuzz
+
+class GameTests: XCTestCase {
+    
+    let game = Game()
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}
